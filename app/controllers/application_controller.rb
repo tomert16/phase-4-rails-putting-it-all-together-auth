a7445944f11c7bb5487b44a5e653775302dc2144
tomer <tomertal52@yahoo.com>
2023-02-09 18:41:06 -0500

    commit tree 984e7017bb251e89a59bec808ed668a0ca97d9e4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  before_action :authorized
+
+  private
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found(error)
+    render json: { errors: errors.full_messages }, status: :not_found  
+  end  
+
+  def authorized
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :recipes, only: [:index, :create]
+  resources :users
+
+  ## CREATE signup route
+  post '/signup', to: 'users#create'
+  ## SHOW auto-login feature route
+  get '/me', to: 'users#show'
+
+  ## CREATE login route
+  post '/login', to: 'sessions#create'
+  ## DESTROY logout route
+  delete '/logout', to: 'sessions#destroy'
+
+  # get '/recipes', to: 'recipes#index'
+  # post '/recipes', to: 'recipes#create'
 end
